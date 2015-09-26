95887cd3b823d368e1e7dab62d5548a1b762b248
Tom Brennan <tommybrennan@gmail.com>
2015-09-26 15:38:48 -0400

    added the projects model and starting laying out the home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :projects
+
   root "pages#home"
+  get "about" => "pages#about"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
